e02806ad3f22b0e6ede898788352d5ad820fc1c6
Darren Wu <wudarre0908@gmail.com>
2022-01-30 18:25:36 -0800

    added page folders to frontend

---
+++ src/frontend/src/onlinePredictionView/OnlinePredictionPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function OnlinePredictionPage() {
+    return <h3>
+        Todo. 
+    </h3>;
+}
+
+export default OnlinePredictionPage;
---
+++ src/frontend/src/plotView/PlotPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function PlotPage() {
+    return <h3>
+        Todo. 
+    </h3>;
+}
+
+export default PlotPage;
