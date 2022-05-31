87d37f257416457e6f9762ba3027bc9aee7b9217
simonfei123 <tfei@ucsd.edu>
2022-05-31 09:48:55 -0700

    Verify Photosensor Data Analyzed the photosensor data and confirmed the timing of the flashes is good.

--- scripts/psychopy_arduino_timing.py
+++ scripts/psychopy_arduino_timing.py
@@ -156,7 +156,9 @@ if use_dsi7:
 
 if use_arduino:
     import serial, threading
-    arduino = serial.Serial(port='COM3', baudrate=115200, timeout=.1)
+    # arduino = serial.Serial(port='COM3', baudrate=115200, timeout=.1)
+    arduino = serial.Serial(port='COM3', baudrate=19200, timeout=.1)
+    # arduino = serial.Serial(port='COM3', baudrate=9600, timeout=.1)
     arduino_call_num = 0
     with open("light_amp.csv", 'w') as csv_file:
         csv_file.write('time, light_amp\n')
@@ -164,7 +166,10 @@ if use_arduino:
         global arduino_call_num
         while True:
             try:
-                data = arduino.readline().decode('ascii')[:-1]
+                # data = arduino.readline().decode('ascii')[:-1]
+                data = int.from_bytes(arduino.read(), "big")
+                if data > 100:
+                    data = 0
                 if arduino_call_num < 100:
                     arduino_call_num+=1
                     continue
@@ -173,8 +178,9 @@ if use_arduino:
                     with open("meta.csv", 'w') as csv_file:
                         csv_file.write('0,0,'+str(time.time()) + '\n')
                 with open("light_amp.csv", 'a') as csv_file:
-                    csv_file.write(data)
-            except UnicodeDecodeError:
+                    # csv_file.write(data)
+                    csv_file.write(str(time.time())+', '+str(data)+'\n')
+            except UnicodeDecodeError and ValueError:
                 pass
     if __name__ == "__main__": 
         # recording_light_amp = multiprocessing.Process(target=record_light_amp,daemon=True)
