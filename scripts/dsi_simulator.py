ffe69bf38b2232028e7bd2b5c415267ac5bf0c3b
chawei217 <chawei217@ucsd.edu>
2022-10-17 21:11:22 -0700

    add dsi_simulator

---
+++ scripts/dsi_simulator.py
@@ -0,0 +1,54 @@
+import time
+import asyncio
+import socketio
+import random
+
+sio = socketio.AsyncClient()
+PORT = 4002
+ISI = 3  # inter-stimulus-interval in seconds
+
+
+@sio.event
+async def connect():
+    print('dsi simulator connected')
+
+
+@sio.event
+async def connect_error(e):
+    print('Connection error:', e)
+
+
+@sio.event
+async def disconnect():
+    print('dsi simulator disconnected')
+
+
+# generate a random letter in the range x - y
+def rand_letter(x='a', y='z'):
+    return chr(random.randint(ord(x), ord(y)))
+
+
+async def _dsi_simulator():
+    """
+
+    :return:
+    """
+    N = 5
+    while True:
+        await sio.sleep(ISI)
+        dsi_message = ''
+        for i in range(N):
+            dsi_message += rand_letter()
+        print('DSI simulator sending message:', dsi_message)
+        await sio.emit('forward_message', dsi_message, namespace='/dsi')
+
+
+async def dsi_simulator():
+    host = '100.112.226.8'
+    await sio.connect(f'http://{host}:{PORT}', namespaces=['/', '/dsi'])
+    await sio.start_background_task(_dsi_simulator)
+    await sio.wait()
+
+
+if __name__ == '__main__':
+    asyncio.run(dsi_simulator())
---
+++ scripts/server.py
@@ -0,0 +1,91 @@
+import sys
+import socketio
+from aiohttp import web
+import json
+from datetime import datetime
+import numpy as np
+from datetime import datetime
+
+
+def reset_content(filepath: str):
+    with open(filepath, "w") as file:
+        json_obj = {'content': []}
+        json.dump(json_obj, file)
+
+
+def get_content(filepath: str):
+    with open(filepath, "r") as file:
+        content = json.load(file)
+        return content
+
+
+def update_content(filepath: str, sender: str, text: str):
+    n_chars_in_line = 60  # number of characters in a line
+    json_obj = get_content(filepath)
+    json_obj['content'].append(
+        {'sender': sender, 'text': text, 'timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+         'n_lines': np.ceil(len(text) / n_chars_in_line) + 1})
+
+    with open(filepath, "w") as file:
+        json.dump(json_obj, file)
+
+
+class DSI(socketio.AsyncNamespace):
+
+    def on_connect(self, sid, environ):
+        print('dsi connected')
+
+    def on_disconnect(self, sid):
+        print('dsi disconnected')
+
+    async def on_forward_message(self, sid, data):
+        message = data
+        print('Received DSI message:', data)
+        # writing message to local back_to_front.json for chat history
+        update_content('states/back_to_front.json', sender='Patient', text=message)
+        # forward message to frontend
+        await self.emit('get_message', message, namespace='/caretaker')
+
+
+class Caretaker(socketio.AsyncNamespace):
+
+    def on_connect(self, sid, environ):
+        print('caretaker connected')
+
+    def on_disconnect(self, sid):
+        print('cartaker disconnected')
+
+    async def on_forward_message(self, sid, data):
+        message = data
+        print('Received caretaker message:', message)
+        # writing message to local back_to_front.json
+        update_content('states/back_to_front.json', sender='Caretaker', text=message)
+
+
+class Server:
+    def __init__(self):
+        # basic AsyncServer setup
+        self.sio = socketio.AsyncServer(async_mode='aiohttp', cors_allowed_origins="*")
+        self.app = web.Application()
+        self.sio.attach(self.app)
+        self.sio.register_namespace(DSI('/dsi'))
+        self.sio.register_namespace(Caretaker('/caretaker'))
+        self.port = 4002
+        self.config = {'patient_id': 12345, 'date': None}
+
+    async def get_config(self, sid):
+        return self.config
+
+    async def display_config(self, sid, data):
+        print(data)
+
+    def start_server(self):
+        reset_content('states/back_to_front.json')
+        self.sio.on('get config', self.get_config)
+        self.sio.on('form submitted', self.display_config)
+        web.run_app(self.app, host='0.0.0.0', port=self.port)  # we're using local host here
+
+
+if __name__ == '__main__':
+    server = Server()
+    server.start_server()
