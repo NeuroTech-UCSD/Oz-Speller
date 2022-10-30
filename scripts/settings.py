2f9cc893075f880137888b14bcdbd87e01163085
simonfei123 <tfei@ucsd.edu>
2022-10-29 19:45:33 -0700

    Edit README 1

---
+++ scripts/settings.py
@@ -0,0 +1,15 @@
+import yaml
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+
+class Configuration:
+    app = {}
+    app['port'] = os.getenv('APP_PORT') if os.getenv('APP_PORT') else 4002
+    app['host'] = os.getenv('HOST_ADDR') if os.getenv('HOST_ADDR') else '192.168.156.132'
+
+    def init(self):
+        pass
